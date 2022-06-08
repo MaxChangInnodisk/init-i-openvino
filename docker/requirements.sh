27d789a325481befd9364f2565190b41af26ed08
MaxChangInnodisk <max_chang@innodisk.com>
2022-06-08 15:20:23 +0800

    feat: add some new requriement

--- docker/requirements.sh
+++ docker/requirements.sh
@@ -9,9 +9,12 @@ apt-get install -qy figlet boxes tree > /dev/null 2>&1
 ROOT=`pwd`
 echo "Workspace is ${ROOT}" | boxes -p a1
 
-# OpenCV
 printd "Install OpenCV " Cy
 apt-get install -qqy libxrender1 libsm6 libxext6 #> /dev/null 2>&1
+
+printd "Install other msicellaneous packages " Cy
+apt-get -qy install bsdmainutils zip
+pip3 install --disable-pip-version-check tqdm cython gdown setuptools packaging pycocotools GPUtil wget colorlog
 
 # For web api
 pip3 install flask flask-socketio flask-cors flasgger gunicorn==20.1.0 eventlet==0.30.2
