7ffbb85507634e88cbda85e79dcc736197d412f0
MaxChangInnodisk <max_chang@innodisk.com>
2022-07-11 14:41:05 +0800

    feat: fix the version of socketio

--- docker/requirements.sh
+++ docker/requirements.sh
@@ -17,6 +17,7 @@ apt-get -qy install bsdmainutils zip jq wget
 pip3 install --disable-pip-version-check tqdm cython gdown setuptools packaging pycocotools GPUtil wget colorlog
 
 # For web api
-pip3 install flask flask-socketio flask-cors flasgger gunicorn==20.1.0 eventlet==0.30.2
+pip3 install flask flask-socketio==5.1.2 flask-cors flasgger gunicorn==20.1.0 eventlet==0.30.2
+pip3 install python-engineio==4.3.2 python-socketio==5.6.0
 apt-get -o Dpkg::Options::="--force-confmiss" install --reinstall netbase
 printd -e "Done${REST}"
