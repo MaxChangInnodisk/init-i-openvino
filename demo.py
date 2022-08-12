f9fdaf51abf65089308a291834cd053d7f5e5ca9
MaxChangInnodisk <max_chang@innodisk.com>
2022-08-12 09:29:51 +0800

    feat: change the FPS outlook and change the annotation

--- demo.py
+++ demo.py
@@ -7,8 +7,17 @@ from ivit_i.utils import Json, Draw
 from ivit_i.utils.logger import config_logger
 from ivit_i.web.ai.pipeline import Source
 from ivit_i.app.handler import get_application
+from ivit_i.web.tools.common import handle_exception
+import math
 
-CV_WIN = "Detection Results"
+FONT_FACE       = cv2.FONT_HERSHEY_COMPLEX_SMALL
+BORDER_FACE     = cv2.LINE_AA
+FONT_SCALE      = 2e-3  # Adjust for larger font size in all images
+THICKNESS_SCALE = 1e-3  # Adjust for larger thickness in all images
+
+CV_WIN          = "Detection Results"
+FULL_SCREEN     = True
+
 alpha_slider_max = 100
 thres = 0
 
@@ -17,6 +26,7 @@ def on_trackbar(val):
     thres = float(val/alpha_slider_max)
 
 def main(args):
+    global FULL_SCREEN
 
     # Instantiation
     json = Json()
@@ -38,12 +48,12 @@ def main(args):
         # Check is openvino and start processes
         if custom_cfg['framework'] == 'openvino':
 
-        # ---------------------------Check model architecture-------------------------------------------------------
+            # Check model architecture
             if 'obj' in dev_cfg[prim_ind]['tag']:
                 from ivit_i.obj import ObjectDetection as trg
 
-                # ---------------------------Check secondary model and loading-------------------------
-                # Get secondary model relative parameter from first json
+                # Check secondary model and loading
+                #   - Get secondary model relative parameter from first json
                 seconlist = [dev_cfg[prim_ind][key] for key in dev_cfg[prim_ind].keys() if "sec" in key]
                 
                 if seconlist != []:
@@ -65,98 +75,103 @@ def main(args):
             if "pose" in dev_cfg[prim_ind]['tag']:
                 from ivit_i.pose import Pose as trg
     
-        # ---------------------------Check input is camera or image and initial frame id/show id----------------------------------------
+            # Check input is camera or image and initial frame id/show id
             src = Source(dev_cfg[prim_ind]['source'], dev_cfg[prim_ind]['source_type'])
 
-            # Create Windows
-            
-            cv2.imshow(CV_WIN, src.get_first_frame())
-            trackbar_name = 'Thres'
-            cv2.createTrackbar(trackbar_name, CV_WIN , 0, alpha_slider_max, on_trackbar)
-            on_trackbar(0)
-
-        # ---------------------------Load model and initial pipeline--------------------------------------------------------------------
+            # Load model and initial pipeline
             trg = trg()  
             model, color_palette = trg.load_model(dev_cfg[prim_ind]) if not ("pose" in dev_cfg[prim_ind]['tag']) else trg.load_model(dev_cfg[prim_ind], src.get_frame()[1])
 
-            has_application=False
+            # Setup Application
+            has_app=False
             try:
                 application = get_application(dev_cfg[prim_ind])
-                has_application = True
-            except Exception as e:
-                logging.error(e)
-                has_application=False
-                application = None
-            
-            try:
+                has_app = False if application == None else True
+                
                 app_info = dev_cfg[prim_ind]["application"]
                 
                 # area_detection have to setup area
-                logging.debug('\n\n\n{}'.format(app_info["name"]))
                 if "area" in app_info["name"]:
                     key = "area_points"
-                    if not key in app_info:
-                        logging.debug("Setting Area")
-                        ret_frame, frame = src.get_frame()
-                        if ret_frame:
-                            logging.debug("Send per frame into application to help it setting")
-                            application.set_area(pnts=None, frame=frame)
-                    else:
-                        logging.debug("Detected Area Points")
-                        application.set_area(pnts=app_info[key])
+                    if not key in app_info: application.set_area(pnts=None, frame=src.get_first_frame())
+                    else: application.set_area(pnts=app_info[key])
 
-                
             except Exception as e:
-                exc_type, exc_obj, exc_tb = sys.exc_info()
-                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-                msg = 'Stream Error: \n{}\n{} ({}:{})'.format(exc_type, exc_obj, fname, exc_tb.tb_lineno)
-                logging.error(msg)
-            
-        # ---------------------------Inference---------------------------------------------------------------------------------------------
+                handle_exception(error=e, title="Could not load application ... set app to None", exit=False)
+                has_app=False
+
+            # Setup CV Windows
+            if not args.server:
+                cv2.namedWindow( CV_WIN, cv2.WND_PROP_FULLSCREEN)
+                cv2.setWindowProperty( CV_WIN, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN if FULL_SCREEN else cv2.WINDOW_NORMAL )
+
+            # Inference
             logging.info('Starting inference...')
-            print("To close the application, press 'CTRL+C' here or switch to the output window and press ESC key")
+            logging.warning("To close the application, press 'CTRL+C' here or switch to the output window and press ESC key")
             
             while True:
-                ret_frame, frame = src.get_frame()
 
-                # 7.2. if not frame break
+                # Get current frame
+                ret_frame, frame = src.get_frame()
+                org_frame = frame.copy()
+                t1 = time.time()
+
+                # Check frame is exist
                 if not ret_frame: 
                     if src.get_type().lower() in ["rtsp", "video"]:
                         src = Source(total_conf['source'], total_conf['source_type'])
                         continue
+                    else:
+                        break
 
-                t1 = time.time()
-
-                org_frame = frame.copy()
-                
-                # update thres
+                # Update thres
                 if thres>0 and (dev_cfg[prim_ind]["openvino"]["thres"] != thres):
                     dev_cfg[prim_ind]["openvino"]["thres"] = thres
 
+                # Do inference
                 info = trg.inference(model, org_frame, dev_cfg[prim_ind])
                 
-        # ---------------------------Drawing detecter to information-----------------------------------------------------------------------
-                if info is not None:
-                    if not has_application or application==None:
-                        logging.debug("No application here")
+                # Drawing detecter to information
+                if not args.server:
+                    
+                    if info is None: continue
+
+                    if not has_app:
                         frame = draw.draw_detections(info, color_palette, dev_cfg[prim_ind])
                     else:
-                        frame = application(org_frame, info)
-                        
-                else:
-                    continue
-        # ---------------------------Show--------------------------------------------------------------------------------------------------              
-                t2 = time.time()
-                cv2.putText(frame, f"FPS:{int(1/(t2-t1))}", (10 , 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 1, cv2.LINE_AA)
+                        frame = application(org_frame, info)            
+                
+                # Show the results
+
+                # Calculate FPS
+                t2              = time.time()
+                fps             = f"FPS:{int(1/(t2-t1))}"
+                height, width   = frame.shape[:2]
+                
+                font_scale      = min(width, height) * FONT_SCALE
+                thickness       = math.ceil(min(width, height) * THICKNESS_SCALE)
+                padding         = 10
+                (text_width, text_height), baseLine = cv2.getTextSize(fps, FONT_FACE, font_scale, thickness)
+                
+
+                cv2.putText(frame, fps, (width - text_width - padding , text_height + baseLine ), FONT_FACE, font_scale, (0, 0, 0), thickness+2, BORDER_FACE)
+                cv2.putText(frame, fps, (width - text_width - padding , text_height + baseLine ), FONT_FACE, font_scale, (0, 255, 255), thickness, BORDER_FACE)
 
                 if not args.server:
+                    
                     cv2.imshow(CV_WIN, frame)
-                
-                    if cv2.waitKey(1) in {ord('q'), ord('Q'), '27'}:
-                        break
+
+                    key = cv2.waitKey(1)
+                    if key in {ord('q'), ord('Q'), 27}:
+                        break                 
+                    elif key in { ord('a'), 201, 206, 210, 214 }:
+                        FULL_SCREEN = not FULL_SCREEN
+                        cv2.setWindowProperty(CV_WIN,cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN if FULL_SCREEN else cv2.WINDOW_NORMAL )
+                    elif key in { ord('c'), 32 }:
+                        pass
+
                 else:
-                    if input('Enter q to leave:') in ['Q', 'q']:
-                        break
+                    logging.info( info["detections"])
                 
             src.release()
 
