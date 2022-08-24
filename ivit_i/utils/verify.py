14c014ff9668a8d367cc631e612353235c85b5f8
MaxChangInnodisk <max_chang@innodisk.com>
2022-08-24 15:29:17 +0800

    chore: add divider in log

--- ivit_i/utils/verify.py
+++ ivit_i/utils/verify.py
@@ -6,18 +6,23 @@ PID         = "196d"
 VID         = "0201"
 
 def innousb():
-    
+
     p = sb.run( f"lsusb | grep {PID}:{VID}", 
                 shell   = True,
                 stdout  = sb.DEVNULL, 
                 stderr  = sb.STDOUT)
 
     if not p.returncode:
-        logging.info(f"Verifying Innodisk Device ... *** Success!!! *** ")
+        msg = f"Verifying Innodisk Device ... Success!!!"
+        logging.info("-"*len(msg))
+        logging.info(msg)
+        logging.info("-"*len(msg))
     else:
-        msg = f"Verifying Innodisk Device ... *** Failed!!! *** "
+        msg = f"Verifying Innodisk Device ... Failed!!!"
         # "Failed !!! Could not find innodisk device."
+        logging.error("*"*len(msg))
         logging.error(msg)
+        logging.error("*"*len(msg))
         sys.exit(errno.EINTR)
 
 if __name__ == '__main__':
