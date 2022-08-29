23c777e1453de0264d0d2dd59d3890f3b85acf7c
MaxChangInnodisk <max_chang@innodisk.com>
2022-08-29 17:06:59 +0800

    feat: provide a new package shell script

--- docker/requirements.sh
+++ docker/requirements.sh
@@ -23,4 +23,8 @@ printd "For Web API " Cy
 pip3 install flask flask-socketio==5.1.2 flask-cors flasgger gunicorn==20.1.0 eventlet==0.30.2
 pip3 install python-engineio==4.3.2 python-socketio==5.6.0
 apt-get -o Dpkg::Options::="--force-confmiss" install --reinstall netbase
+
+# For Package
+pip3 install merak
+
 printd "Done"
--- release.sh
+++ release.sh
@@ -1,1 +1,21 @@
-python3 setup.py build_ext --inplace --src ivit_i --exclude '__inti__|web'
+#!/bin/bash
+
+ROOT=$(pwd)
+API="ivit_i"
+WEB="web"
+
+OUTPUT="./out"
+
+# Exclude Web API
+mv -f "${API}/${WEB}" "${ROOT}/"
+
+# Start to cythonize via merak
+merak cythonize "${API}" "${OUTPUT}"
+
+# Remove whole ivit_i folder and create a new one
+rm -rf "${API}" && mv -f "${OUTPUT}/${API}" "${ROOT}"
+
+# Move so api file and web api into ivit_i folder
+mv -f "${ROOT}/${WEB}"  ${API}
+
+echo "Done !!!!"
