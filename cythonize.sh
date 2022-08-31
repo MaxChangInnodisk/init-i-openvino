91a24e38d95e352d9be7f27b9f4fd0830f0a05a6
MaxChangInnodisk <max_chang@innodisk.com>
2022-08-31 14:37:57 +0800

    feat: add more double check when cythonize

--- cythonize.sh
+++ cythonize.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
 ROOT=$(pwd)
-API="ivit_i"
+API="bk"
 WEB="web"
 
 OUTPUT="./out"
 
 # Exclude Web API
-mv -f "${API}/${WEB}" "${ROOT}/"
+if [[ -d "${API}/${WEB}" ]];then 
+    echo "Move out Web API folder"
+    mv -f "${API}/${WEB}" "${ROOT}/"; 
+fi
 
 # Start to cythonize via merak
-ln -s /usr/bin/python3 /usr/bin/python
+if [[ -z $(which python) ]];then 
+    echo "Could not find python, link python3 to python"
+    ln -s /usr/bin/python3 /usr/bin/python; 
+fi
+
+echo "Start to cythonize ivit_i ... "
 merak cythonize "${API}" "${OUTPUT}"
 
 # Remove whole ivit_i folder and create a new one
-rm -rf "${API}" \
-&& mv -f "${OUTPUT}/${API}" "${ROOT}" \
-&& rm -rf out
+rm -rf "${API}"
+mv -f "${OUTPUT}/${API}" "${ROOT}"
+rm -rf out
 
 # Move so api file and web api into ivit_i folder
-mv -f "${ROOT}/${WEB}"  ${API}
+mv -f "${ROOT}/${WEB}" ${API}
 
 # Change Owner
 chown 1000:1000 -R /workspace
