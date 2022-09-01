8fddb93d6c501045bbddab6aa4852a8317e7de94
MaxChangInnodisk <max_chang@innodisk.com>
2022-09-01 11:23:37 +0800

    fix: install pyinstaller in python3

--- docker/utils.sh
+++ docker/utils.sh
@@ -62,6 +62,6 @@ function check_pyinstaller(){
 	# Install pyinstaller for inno-verify
 	if [[ -z $(which pyinstaller) ]];then
 		printd "Installing pyinstaller for inno-verify .... " Cy
-		pip install setuptools pyinstaller -q
+		pip3 install setuptools pyinstaller -q
 	fi
 }
